7f840efbc01361dafb104bf13364b1430dbb0023
Oskar <kogutoskar123@gmail.com>
2019-04-17 20:24:15 +0200

    Added GameOver scene

---
+++ EndlessBlockBreaker/Assets/Code/app.Controllers/GameStateController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace app.Controllers {
+    public static class GameStateController {
+        public static void EndGame() {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
