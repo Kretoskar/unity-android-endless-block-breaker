5ca1cb6c407034f635bbacbb46b06a53c8281cd7
Oskar <kogutoskar123@gmail.com>
2019-04-19 20:26:51 +0200

    Added block prefabs

---
+++ EndlessBlockBreaker/Assets/Code/app.Gameplay/app.Gameplay.Threats/Threat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace app.Gameplay.Threats {
+    /// <summary>
+    /// Base class for all the threats
+    /// </summary>
+    public abstract class Threat : MonoBehaviour {
+        [SerializeField]
+        protected int _score;
+        [SerializeField]
+        protected int _speed;
+
+        /// <summary>
+        /// Add player score after death
+        /// </summary>
+        protected void AddScore() {
+            print(_score);
+        }
+
+        /// <summary>
+        /// Move after spawn
+        /// </summary>
+        protected abstract void Move();
+
+        /// <summary>
+        /// Behaviour when threat is killed
+        /// </summary>
+        protected abstract void Die();
+    }
+}
