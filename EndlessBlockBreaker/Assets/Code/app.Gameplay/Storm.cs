0f19ee3aeab1025d8231c4e7ea498673c417d8f4
Oskar <kogutoskar123@gmail.com>
2019-07-27 16:16:00 +0200

    Added storm fading

--- EndlessBlockBreaker/Assets/Code/app.Gameplay/Storm.cs
+++ EndlessBlockBreaker/Assets/Code/app.Gameplay/Storm.cs
@@ -16,17 +16,41 @@ namespace app.Gameplay {
         public void StartStorm(Transform ballTransform) {
             if (!_isStorm) {
                 _isStorm = true;
+
                 StartCoroutine(StormCoroutine(ballTransform));
+
             }
         }
 
         private IEnumerator StormCoroutine(Transform ballTransform) {
             GameObject storm = Instantiate(_storm);
             GameObject lightning = Instantiate(_lightning, ballTransform.position, Quaternion.identity);
+
+            SpriteRenderer stormSpriteRenderer = storm.GetComponent<SpriteRenderer>();
+            SpriteRenderer lightningSpriteRenderer = lightning.GetComponent<SpriteRenderer>();
+
+            StartCoroutine(FadeOut(stormSpriteRenderer));
+            StartCoroutine(FadeOut(lightningSpriteRenderer));
             yield return new WaitForSeconds(_stormTimeInSeconds);
+
             Destroy(storm);
             Destroy(lightning);
             _isStorm = false;
+        }
+
+        private IEnumerator FadeOut(SpriteRenderer spriteRenderer) {
+            Color spriteColor = spriteRenderer.color;
+
+            while(spriteColor.a > 0f) {
+                spriteColor.a -= Time.deltaTime / _stormTimeInSeconds;
+                spriteRenderer.color = spriteColor;
+
+                if (spriteColor.a <= 0f)
+                    spriteColor.a = 0.0f;
+
+                yield return null;
+            }
+            spriteRenderer.color = spriteColor;
         }
 
     }
